b57659bf4e4d68b94c5f2a5a93df1036007588ef
HuitaePark <qkrgmlxo3174@gmail.com>
2024-07-23 16:36:33 +0900

    싱글톤 완강

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,13 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+    private int price;
+    private void order(String name,int price){
+        System.out.println(name+""+price);
+        this.price = price;
+    }
+    public int getPrice(){
+        return price;
+    }
+}
