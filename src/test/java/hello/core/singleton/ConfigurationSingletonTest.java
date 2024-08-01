6d460d8786c5afd22d2023e91aca2589df4d4cf9
HuitaePark <qkrgmlxo3174@gmail.com>
2024-08-02 01:02:10 +0900

    싱글톤 완료

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -3,13 +3,12 @@ package hello.core.singleton;
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class ConfigurationSingletonTest {
 
@@ -18,7 +17,7 @@ public class ConfigurationSingletonTest {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberServiceImpl memberService = ac.getBean("memberService",MemberServiceImpl.class);
-        OrderServiceImpl orderService = ac.getBean("orderservice",OrderServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService",OrderServiceImpl.class);
         MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
 
