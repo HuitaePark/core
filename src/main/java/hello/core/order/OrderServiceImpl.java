ce5211625525b8b22da159078338ecd55216fe1c
HuitaePark <qkrgmlxo3174@gmail.com>
2024-08-12 01:20:55 +0900

    lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,18 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
+    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
-        this.discountPolicy = discountPolicy;
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -24,8 +22,5 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member,itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
     }
 }
